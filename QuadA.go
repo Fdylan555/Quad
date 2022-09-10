e5b1557d5accba1966a580226a6f1ef138cfb583
root <root@LAPTOP-EOGHAN.localdomain>
2022-09-10 20:07:08 +0200

    QuadA

--- QuadA.go
+++ QuadA.go
@@ -7,21 +7,24 @@ import (
 func QuadA(x, y int) {
 	if x > 0 && y > 0 {
 		z01.PrintRune('o')
-		for ligne1 := 1; ligne1 < x+1; ligne1++ {
+		for ligne1 := 1; ligne1 < x-1; ligne1++ {
 			z01.PrintRune(45)
 		}
 		z01.PrintRune('o')
 		z01.PrintRune('\n')
-
-		for ligne2 := 1; ligne2 < y+1; ligne2++ {
-
+		for ligne2 := 1; ligne2 < y-1; ligne2++ {
 			z01.PrintRune('|')
-			z01.PrintRune(' ')
+
+			for espace := 2; espace <= x-1; espace++ {
+				z01.PrintRune(' ')
+
+			}
 			z01.PrintRune('|')
 			z01.PrintRune('\n')
 		}
+
 		z01.PrintRune('o')
-		for ligne3 := 1; ligne3 < y+1; ligne3++ {
+		for ligne3 := 1; ligne3 < x-1; ligne3++ {
 			z01.PrintRune(45)
 		}
 
