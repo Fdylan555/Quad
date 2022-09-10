f8ae4ce51e337be93b37d2e617728d9dc7345c16
root <root@LAPTOP-EOGHAN.localdomain>
2022-09-10 23:07:01 +0200

    QuadE

---
+++ QuadE.go
@@ -0,0 +1,32 @@
+package piscine
+
+import "github.com/01-edu/z01"
+
+func QuadE(x, y int) {
+	if x > 0 && y > 0 {
+		z01.PrintRune('A')
+		for ligne1 := 1; ligne1 < x-1; ligne1++ {
+			z01.PrintRune('B')
+		}
+		z01.PrintRune('C')
+		z01.PrintRune('\n')
+		for ligne2 := 1; ligne2 < y-1; ligne2++ {
+			z01.PrintRune('B')
+
+			for espace := 2; espace <= x-1; espace++ {
+				z01.PrintRune(' ')
+
+			}
+			z01.PrintRune('B')
+			z01.PrintRune('\n')
+		}
+
+		z01.PrintRune('C')
+		for ligne3 := 1; ligne3 < x-1; ligne3++ {
+			z01.PrintRune('B')
+		}
+
+		z01.PrintRune('A')
+		z01.PrintRune('\n')
+	}
+}
